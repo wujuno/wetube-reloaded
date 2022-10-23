bc22337af801f019c8056476012dbbed7486a8c3
wujuno <86146780+wujuno@users.noreply.github.com>
2022-10-23 13:37:58 +0900

    heroku PORT

--- src/init.js
+++ src/init.js
@@ -5,7 +5,7 @@ import "./models/user";
 import "./models/comment";
 import app from "./server"
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 /* 4000PORT로 들어오는 request를 감지하고 handelListening 함수를 실행시킨다. */
