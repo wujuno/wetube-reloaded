f4745964670135cc21f0168beb957b6faf50e8f6
wujuno <86146780+wujuno@users.noreply.github.com>
2022-10-23 23:23:25 +0900

    s3 uploads

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,14 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+    credentials: {
+        accessKeyId: process.env.AWS_ID,
+        secretAccessKey: process.env.AWS_SECRET
+    }
+
+})
 
 
 export const localsMiddleware = (req, res, next) => {
@@ -26,18 +36,30 @@ export const publicOnlyMiddleware = (req,res,next) => {
     }
 }
 
+const multerUploader = multerS3({
+    s3: s3,
+    bucket: 'wetube-wujuno',
+    acl: "public-read",
+}
+)
 export const avatarUpload = multer({ 
     dest: "uploads/avatars/", 
     limits:{
         fileSize : 3000000,
-}});
+    },
+    storage:multerUploader
+});
 export const videoUpload = multer({ 
     dest: "uploads/videos/", 
     limits:{
         fileSize : 10000000,
-}});
+    },
+    storage:multerUploader
+});
 export const thumbnailUpload = multer({ 
     dest: "uploads/videos/", 
     limits:{
         fileSize : 10000000,
-}});
+    },
+    storage:multerUploader
+});
